dc0c67c2f62e3377bda3681f477d4d935fcca5a9
Yuri U <y.unterreiner@outlook.com>
2020-12-02 19:05:59 -0600

    Correct harvester module and add flexbox gridding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,25 +6,23 @@ import {plantSeeds} from "./tractor.js";
 import {addPlant} from "./field.js";
 import { usePlants} from "./field.js";
 import {harvestPlants} from "./harvester.js"
+import {listHarvest} from "./harvester.js"
+
 
 
 
 // Create yearly plan
 const yearlyPlan = createPlan();
-console.log(yearlyPlan)
 
 // Plant seeds
 const seeds = plantSeeds(yearlyPlan);
-
 // Add Plant
 addPlant(seeds)
-
 // Use Plants
 let plants = usePlants();
-
-console.log(plants)
 
 // Harvest plants
 let seedObj = harvestPlants(plants);
 
-console.log(seedObj)
+// Display harvested plants
+listHarvest(seedObj)
